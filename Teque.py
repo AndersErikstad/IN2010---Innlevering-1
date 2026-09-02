ad4b55bbd65ff7667d49db7d9b7df09d1841aadc
Anders Erikstad <anderserikstad@Anders-sin-MacBook-Pro.local>
2026-09-02 11:59:26 +0200

    utkast?

--- Teque.py
+++ Teque.py
@@ -3,3 +3,17 @@
 def adam(x):
     pass
 
+
+
+def teque(input:str):
+    input_as_list = input.strip().split("\n")
+    for operation in range(1, int(input_as_list[0])):
+        operation_and_num = operation.split()
+        x = int(input_as_list[operation][1])
+        if input_as_list[operation][0] == "get":
+            return
+        elif input_as_list[operation][0] == "push_front":
+
+        elif input_as_list[operation][0] == "push_middle":
+
+        elif input_as_list[operation][0] == "push_back":
