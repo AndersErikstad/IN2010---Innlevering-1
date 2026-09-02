9c2cf9844ea4161296db9aa9d5cbb42ab7fc2c44
Anders Erikstad <anderserikstad@Anders-sin-MacBook-Pro.local>
2026-09-02 12:13:00 +0200

    litt mer kjøtt lagt til før forelesning. Tenker vi lager egen liste struktur?

--- Teque.py
+++ Teque.py
@@ -1,17 +1,32 @@
+from pyarrow import nulls
 
 
 def adam(x):
     pass
 
+# Tenker vi lager vår egen liste?
+class LinkedList:
+    def __init__(self):
+
+
+class Node:
+    def __init__(self, content, next: 'Node'):
+        self.content = content
+        self.next = next
+    def get_content(self):
+        return self.content
+    def get_next(self):
+        return self.next
 
 
 def teque(input:str):
+
     input_as_list = input.strip().split("\n")
     for operation in range(1, int(input_as_list[0])):
         operation_and_num = operation.split()
         x = int(input_as_list[operation][1])
         if input_as_list[operation][0] == "get":
-            return
+
         elif input_as_list[operation][0] == "push_front":
 
         elif input_as_list[operation][0] == "push_middle":
