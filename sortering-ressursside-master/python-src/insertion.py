257d80d2c02315b7825d82a78d5d26d648e60846
Anders Erikstad <anderserikstad@Anders-sin-MacBook-Pro.local>
2026-09-11 16:03:31 +0200

    more optimized insertion sort algo

--- sortering-ressursside-master/python-src/insertion.py
+++ sortering-ressursside-master/python-src/insertion.py
@@ -2,15 +2,21 @@ def sort(A):
     # Do insertion sort here. Use the Sorter's comparison- and swap
     # methods for automatically counting the swaps and comparisons.
 
+    #for i in range(1, len(A)):
+    #    j = i - 1
+    #    while j>= 0:
+    #        if A[i]<A[j]:
+    #            A.swap(i, j)
+    #            j -= 1
+    #            i = j + 1
+    #        else:
+    #            break
+
     for i in range(1, len(A)):
-        j = i - 1
-        while j>= 0:
-            if A[i]<A[j]:
-                A.swap(i, j)
-                j -= 1
-                i = j + 1
-            else:
-                break
+        j = i
+        while (j > 0) and (A[j] < A[j - 1]):
+            A.swap(j, j - 1)
+            j -= 1
 
     # Use A.swap(i, j) to swap the values at two indices i and j. The swap is
     # counted, when using this method. Comparisons are counted automatically.
